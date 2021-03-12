b8c45e10cbac8134564acdebbef632ba4c35cf42
paoli7612 <paoli7612@gmail.com>
2021-03-12 18:13:22 +0100

    ricominciando

--- makefile
+++ makefile
@@ -1,39 +1,19 @@
-run: bin/run.out
-	./bin/run.out
+GAME_C = src/game.cpp
 
-editor: bin/editor.out
-	./bin/editor.out
+GAME_OUT = bin/game.out
+EDITOR_OUT = bin/editor.out
 
-bin/run.out: bin/map.o bin/game.o bin/player.o
-	g++ src/main_run.cpp \
-		bin/map.o \
-		bin/game.o \
-		bin/player.o \
-		-lsfml-graphics -lsfml-window -lsfml-system \
-			-o bin/run.out
+run: $(GAME_OUT)
+	./$(GAME_OUT)
 
-bin/editor.out: bin/map.o bin/editor.o
-	g++ src/main_editor.cpp \
-		bin/map.o \
-		bin/editor.o \
-		-lsfml-graphics -lsfml-window -lsfml-system \
-			-o bin/editor.out
-
-tests: bin/map.o
-	g++ tests/create_map.cpp bin/map.o -o bin/create_map.out
-	g++ tests/grass_map.cpp bin/map.o -o bin/grass_map.out
-
-bin/map.o: src/map.cpp
-	g++ src/map.cpp -c -o bin/map.o
-
-bin/editor.o: src/editor.cpp
-	g++ src/editor.cpp -c -o bin/editor.o
-
-bin/game.o: src/game.cpp
-	g++ src/game.cpp -c -o bin/game.o
-
-bin/player.o: src/player.cpp
-	g++ src/player.cpp -c -o bin/player.o
+editor: $(EDITOR_OUT)
+	./$(EDITOR_OUT)
 
 clean:
 	rm bin/*
+
+$(GAME_OUT): $(GAME_C)
+	g++ $(GAME_C) -o $(GAME_OUT)
+
+$(EDITOR_OUT): $(EDITOR_C)
+	g++ $(EDITOR_C) -o $(EDITOR_OUT)
--- src/game.cpp
+++ src/game.cpp
@@ -1,109 +1,11 @@
+#include <iostream>
 
-#include "../include/game.h"
+using namespace std;
 
-void Game::start()
+int main(int argc, char **argv)
 {
-    map_load(map, "spawn");
 
-    window.create(sf::VideoMode(800, 800), "Carcadia - editor", sf::Style::Fullscreen);
-    sf::Vector2u size = window.getSize();
+    cout << "Hello world" << endl;
 
-    player.start(size.x/2, size.y/2);
-    player.setMap(map);
-
-    textures[INTERIOR].loadFromFile("img/interior.png");
-    textures[OUTSIDE].loadFromFile("img/outside.png");
-    textures[TERRAIN].loadFromFile("img/terrain.png");
-
-    for (int i=0; i<3; i++)
-        tile[i].setTexture(textures[i]);
-
-    loop();
-}
-
-void Game::loop()
-{
-    running = true;
-    while (running)
-    {
-        // Tick
-        if (clock.getElapsedTime().asSeconds() >= 1.0f / 60){
-            dt = clock.getElapsedTime().asSeconds();
-            clock.restart();
-        } else continue;
-
-        event();
-        update();
-        draw();
-    }
-}
-
-void Game::event()
-{
-    sf::Event event;
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        player.up();
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        player.down();
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        player.right();
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        player.left();
-    else player.stand();
-
-    while (window.pollEvent(event))
-    {
-        switch (event.type)
-        {
-            case sf::Event::KeyPressed: {
-                switch (event.key.code) {
-                    case sf::Keyboard::Escape:
-                        running = false;
-                        break;
-                }
-                break;
-            }
-            case sf::Event::Closed:
-                running = false;
-                break;
-        }
-    }
-}
-
-void Game::update()
-{
-    player.update(dt);
-}
-
-void Game::draw()
-{
-    sf::View view(sf::FloatRect(0, 0, 800, 500));
-    view.setCenter(player.getPosition());
-
-    window.clear(sf::Color(100, 50, 120));
-    window.setView(view);
-
-    for (int y=0; y<HEIGHT; y++)
-        for (int x=0; x<WIDTH; x++)
-        {
-            tile_t t = map.tiles[y][x];
-            for (int z=0; z<DEPTH; z++)
-            {
-                image_t image = t.image[z];
-                if (image.ix == 7612 && image.iy == 7612)
-                    continue;
-                kind_t kind = image.kind;
-                tile[kind].setTextureRect(sf::IntRect(image.ix*32, image.iy*32, 32, 32));
-                tile[kind].setPosition(x*32, y*32);
-                window.draw(tile[kind]);
-            }
-        }
-
-    window.setView(view);
-    window.draw(player);
-
-    view.setCenter(0, 0);
-
-    window.display();
+    return 0;
 }
