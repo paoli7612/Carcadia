a18466cfb12646c819fbb6fe2952872c11934a07
paoli7612 <paoli7612@gmail.com>
2021-03-13 19:57:16 +0100

    draw map in game

--- src/game.cpp
+++ src/game.cpp
@@ -5,11 +5,13 @@
 
 class Game {
     public:
-        const int WIDTH = 1080, HEIGHT = 720;
         const std::string TITLE = "Carcadia - paoli7612";
         Player player;
 
         map_t map;
+
+        sf::Texture images_texture;
+        sf::Sprite images_sprite;
 
         sf::RenderWindow window;
         sf::Clock clock; float dt;
@@ -17,9 +19,12 @@ class Game {
 
         Game()
         {
-            window.create(sf::VideoMode(WIDTH, HEIGHT), TITLE);
+            window.create(sf::VideoMode(WIDTH*32, HEIGHT*32), TITLE);
             player.init();
             map_load(map, "spawn");
+
+            images_texture.loadFromFile("img/tiles.png");
+            images_sprite.setTexture(images_texture);
         }
 
         void start()
@@ -57,7 +62,7 @@ class Game {
 
             sf::Event event;
             while (window.pollEvent(event))
-            {
+            {  
                 switch (event.type)
                 {
                     case sf::Event::Closed:
@@ -82,14 +87,34 @@ class Game {
             window.clear();
             
             draw_grill();
+            draw_map();
+
             window.draw(player);
             window.display();
         }
 
+
+        void draw_map()
+        {
+            for (int z=0; z<DEPTH; z++)
+                for (int y=0; y<HEIGHT; y++)
+                    for (int x=0; x<WIDTH; x++)
+                    {
+                        std::cout << x << std::endl;
+                        image_t &image = map.tiles[y][x].image[z];
+                        if (!image_equals(image, EMPTY))
+                        {
+                            images_sprite.setTextureRect(sf::IntRect(image.ix*32, image.iy*32, 32, 32));
+                            images_sprite.setPosition(sf::Vector2f(x*32, y*32));
+                            window.draw(images_sprite);
+                        }
+                    }
+        }
+
         void draw_grill()
         {
-            const int vertical_lines = (WIDTH/TILE)*4;
-            const int horizontal_lines = (HEIGHT/TILE)*4;
+            const int vertical_lines = (WIDTH*32/TILE)*4;
+            const int horizontal_lines = (HEIGHT*32/TILE)*4;
 
             sf::Vertex *v_lines = new sf::Vertex[vertical_lines];
             sf::Vertex *h_lines = new sf::Vertex[horizontal_lines];
@@ -97,12 +122,12 @@ class Game {
             for (int i=0; i<40*2; i+=2)
             {
                 v_lines[i] = sf::Vertex(sf::Vector2f(i/2*TILE, 0));
-                v_lines[i+1] = sf::Vertex(sf::Vector2f(i/2*TILE, HEIGHT));
+                v_lines[i+1] = sf::Vertex(sf::Vector2f(i/2*TILE, HEIGHT*32));
             }
             for (int i=0; i<25*2; i+=2)
             {
                 h_lines[i] = sf::Vertex(sf::Vector2f(0, i/2*TILE));
-                h_lines[i+1] = sf::Vertex(sf::Vector2f(WIDTH, i/2*TILE));
+                h_lines[i+1] = sf::Vertex(sf::Vector2f(WIDTH*32, i/2*TILE));
             }
 
             window.draw(v_lines, vertical_lines, sf::Lines);
