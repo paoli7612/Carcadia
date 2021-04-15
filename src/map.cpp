221851361998c74838a4e3a2375711edb00605fb
paoli7612 <paoli7612@gmail.com>
2021-04-15 15:41:31 +0200

    init map

--- src/map.cpp
+++ src/map.cpp
@@ -2,12 +2,25 @@
 
 void init(map_t &map, const char title[10], const int width, const int height)
 {
+    strcpy(map.title, title);
+
+    map.width = width;
+    map.height = height;
+
+    map.n_doors = 0;
+
+    map.tiles = new tile_t*[height];
+    for (int y=0; y<height; y++)
+        map.tiles[y] = new tile_t[width];
     
-}
-
-void print(const map_t &map)
-{
-
+    for (int y=0; y<map.height; y++)
+        for (int x=0; x<map.width; x++)
+        { // per ogni tile della matrice
+            tile_t &tile = map.tiles[y][x];
+            for (int z=0; z<DEPTH; z++)
+                tile.images[z] = EMPTY_IMAGE;
+            tile.isCollide = false;    
+        }
 }
 
 void load(map_t, char[10])
