e1be02a3d8fd8ece8c4b3c48a2dc607f80f6eb2a
paoli7612 <paoli7612@gmail.com>
2021-04-22 18:21:02 +0200

    is solid function in map

--- src/map.cpp
+++ src/map.cpp
@@ -187,3 +187,11 @@ void map_set_solid(map_t &map, const int x, const int y, const bool col=true)
 
     map.tiles[y][x].isCollide = col;
 }
+
+bool is_solid(const map_t &map, const int x, const int y)
+{
+    if (! in_map(map, x, y))
+        return true;
+    
+    return map.tiles[y][x].isCollide;
+}
