1b35d79b97250f6bdf01f020f05d2171ebadde2d
paoli7612 <paoli7612@gmail.com>
2021-04-28 14:46:35 +0200

    con calma

--- tests/map_door.cc
+++ tests/map_door.cc
@@ -32,15 +32,42 @@ int main(int argc, char const *argv[])
     {
         map_print(map);
         cout << menu << endl;
-        int scelta;
+
+        int choice;
         cout << "-> ";
-        cin >> scelta;
+        cin >> choice;
 
+        switch (choice)
+        {
+            case 1: // add
+                {
+                    door_t door;
+                    cout << "dest: ";
+                    cin >> door.dest;
+
+                    cout << "x y: ";
+                    cin >> door.x >> door.y;
+                
+                    cout << "image (x y): ";
+                    cin >> door.image.x >> door.image.y;
+                
+                    cout << "dx dy: ";
+                    cin >> door.dx >> door.dy;
+                    map_add_door(map, door);
+                }
+                break;
+            case 2: // edit
+                break;
+            case 3: // delete
+                break;
+
+            case 4: // save and exit
+                map_save(map);
+                runnning = false;
+                break;
+        }
     }
-    
 
-
-    
 
     return 0;
 }
