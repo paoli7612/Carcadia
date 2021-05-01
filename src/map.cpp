b543587c28d3ea83ee4b2569151b7680d9090679
paoli7612 <paoli7612@gmail.com>
2021-05-01 11:25:56 +0200

    door in a new cpp file

--- src/map.cpp
+++ src/map.cpp
@@ -1,5 +1,7 @@
 #include "../include/map.h"
 
+#include "map/door.cpp"
+
 image_t EMPTY_IMAGE = {-1, -1};
 
 bool is_empty(const image_t image)
@@ -182,38 +184,6 @@ void map_delete(const char title[10])
     std::remove(filename.c_str());
 }
 
-void map_add_door(map_t &map, const door_t door)
-{
-    if (map.n_doors == 0)
-    {
-        map.doors = new door_t[1];
-        map.doors[0] = door;
-        map.n_doors = 1;
-    }
-    else
-    {
-        // copio il puntatore alle porte attuali
-        door_t *old_doors = map.doors;          
-
-        // uso il puntatore della mappa per creare un'array lungo come prima +1
-        map.doors = new door_t[map.n_doors + 1];    
-        
-        // ricopio tutte le porte nel nuovo array
-        for (int i=0; i<map.n_doors; i++)
-            map.doors[i] = old_doors[i];
-        
-        // aggiungo la nuova porta e 
-        map.doors[map.n_doors] = door;
-
-        //incremento il contatore delle porte nella mappa
-        map.n_doors++;
-
-        // elimino il vecchio array di porte
-        delete [] old_doors;
-    }
-    
-}
-
 void map_add_image(map_t &map, const int x, const int y, const image_t image)
 {
     if (!in_map(map, x, y))
@@ -263,53 +233,4 @@ bool map_is_solid(const map_t &map, const int x, const int y)
         return true;
     
     return map.tiles[y][x].isCollide;
-}
-
-void map_rem_door(map_t &map, const int n)
-{
-    if (map.n_doors == 1)
-    {
-        delete [] map.doors;
-        map.n_doors = 0;
-    }
-
-    // copio il puntatore alle porte attuali
-    door_t *old_doors = map.doors;          
-
-    // uso il puntatore della mappa per creare un'array lungo come prima -1
-    map.doors = new door_t[map.n_doors - 1];    
-    
-    // ricopio tutte le porte precedenti a quella eliminata
-    for (int i=0; i<n; i++)
-        map.doors[i] = old_doors[i];
-    
-    // ricopio tutte le porte dopo quella eliminata
-    for (int i=n; i<map.n_doors; i++)
-        map.doors[i] = old_doors[i+1];
-    
-    //decremento il contatore delle porte nella mappa
-    map.n_doors--;
-
-    // elimino il vecchio array di porte
-    delete [] old_doors;
-}
-
-void map_rem_door(map_t &map, const int x, const int y)
-{
-    int dd = map_get_door(map, x, y);
-    if (dd >= 0)
-        map_rem_door(map, dd);
-}
-
-int map_get_door(map_t &map, const int x, const int y)
-{
-    // ritorna la posizione nell'array delle door della door a posizione x y
-    // ... se non sono presenti doors a quella posizione ritorna -1
-    for (int i=0; i<map.n_doors; i++)
-    {
-        door_t &door = map.doors[i];
-        if (door.x == x && door.y == y)
-            return i;
-    }
-    return -1;
 }
