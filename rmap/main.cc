798b01686ecc71cfd7b9dc5bd46903d69454c2b8
paoli7612 <paoli7612@gmail.com>
2021-05-09 19:56:31 +0200

    map_t separato

---
+++ rmap/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+#include "map.h"
+
+int main(int argc, char const *argv[])
+{
+
+
+    return 0;
+}
---
+++ rmap/map.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_MAP_H_
+#define INCLUDE_MAP_H_
+
+#include <iostream>
+#include <fstream>
+
+struct map_t {
+
+};
+
+void map_print(const map_t &map);
+
+#endif /* INCLUDE_MAP_H_ */
