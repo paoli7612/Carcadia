1890470079bac78530ba603124554a7918a26695
paoli7612 <paoli7612@gmail.com>
2021-05-09 20:13:02 +0200

    map save width and height

--- rmap/main.cpp
+++ rmap/main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 
+    map_t map;
+    map_load(map, "prima");
+
 
     return 0;
 }
---
+++ rmap/map.cpp
@@ -0,0 +1,28 @@
+#include "map.h"
+
+void map_load(map_t &map, title_t title)
+{
+    std::string filename = "maps/" + (std::string)title + ".tomaoli";
+    std::ifstream file(filename);
+
+    if (! file)
+        throw "map not found";
+
+    
+
+    file.close();
+}
+
+void map_save(const map_t &map)
+{
+    std::string filename = "maps/" + (std::string)map.title + ".tomaoli";
+    std::ofstream file(filename);
+
+    if (! file)
+        throw "map not found";
+
+    file.write((char*)&map.width, sizeof(int));
+    file.write((char*)&map.height, sizeof(int));
+    
+    file.close();
+}
